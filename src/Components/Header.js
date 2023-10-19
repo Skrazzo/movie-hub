6528a0f699a23d7c10a401b4052bee3d2b2df493
Leons Aleksandrovs <skrazzo@proton.me>
2023-10-19 22:29:09 +0300

    Open dialog

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,32 @@
-import { IconMenu2 } from '@tabler/icons-react';
-import React from 'react';
+import { IconMenu2, IconX } from '@tabler/icons-react';
+import {React, useState} from 'react';
 import '../scss/Header.scss';
 
 export default function Header(props) {
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     return (
-        <div className='header'>
-            <div>
-                <img src={props.pfp}/>
-                <span>{props.username}</span>
+        
+        <>
+            <div className={(dialogOpen) ? 'header-dialog' : ''}>
+                <dialog open={dialogOpen}>
+                    <div className={'title'}>
+                        <span>Search movies</span>
+                        <IconX onClick={() => setDialogOpen(false)}/>
+                    </div>
+
+                    <div className={'content'}>
+
+                    </div>
+                </dialog>
             </div>
-            <div><IconMenu2 size={35}/></div>
-        </div>
+            <div className='header'>
+                <div>
+                    <img src={props.pfp}/>
+                    <span>{props.username}</span>
+                </div>
+                <div><IconMenu2 onClick={() => setDialogOpen(true)} size={35}/></div>
+            </div>
+        </>
     );
 }
