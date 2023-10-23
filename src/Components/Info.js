364cdcfcc52b0b14df3e868ee2d5ef333a809d5d
Lxgends <marcitis24@gmail.com>
2023-10-23 11:34:34 +0300

    Added movie data
    Leons resns kroplis

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../scss/Info.scss';
+import axios from 'axios';
+
 
 export default function Info() {
+    const [movieData, setMovieData] = useState({title: '', image: ''});
+    useEffect(() => {
+        axios.get('get_index_info.php').then((res) => {
+            
+            if(res.data.code === 0){
+                alert("Error: "+ res.data.reason);
+            }else{
+                console.log(res.data.reason);
+            }
+            setMovieData({
+                title: res.data.movie_title,
+                image: res.data.movie_img,
+            });
+        });
+    }, []);
     return (
         <>
-            <div class="infocontainer">
-                <div class="moviereview"></div>
+            <div className="infocontainer">
+                <div className="moviereview">
+                    <img src={movieData.movie_img} alt={movieData.movie_title} />
+                    <h1>{movieData.movie_title}</h1>
+                </div>
             </div>
         </>
     )
