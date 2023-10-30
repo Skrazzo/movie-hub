66eb1bf8b2c5054d3e786a7910b403ae2bcfc481
Leons Aleksandrovs <skrazzo@proton.me>
2023-10-30 12:12:35 +0200

    done

--- src/Routes/Review.js
+++ src/Routes/Review.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import '../scss/Review.scss';
 import StarRating from '../Components/StarRating';
 import { object_to_urlsearchparams } from '../functions';
+
 
 
 export default function Review() {
@@ -12,7 +13,13 @@ export default function Review() {
     const [requestPending, setRequestPending] = useState(false);
     // Use the useParams hook to access route parameters
     const { tmdb_id } = useParams();
+    
+    const [infoContainerHeight, setInfoContainerHeight] = useState(0);
+    const infoContainerRef = useRef(null);
 
+    
+    
+        
     function uploadRating(rating){
         // rating has changed, so that means we need to update the database
         const data = {
@@ -41,23 +48,33 @@ export default function Review() {
                 setMovieData(res.data.reason);
             }
         });
+
+        setInfoContainerHeight(infoContainerRef.current.clientHeight);
+        setTimeout(() => {
+            setInfoContainerHeight(infoContainerRef.current.clientHeight);
+        }, 2500);
     }, []);
 
     useEffect(() => {
         console.log('Current request:', requestPending);
-
+        
     }, [requestPending]);
 
     return (
         <div className=''>
             {/* <img src={window.baseImgUrl + movieData.backdrop}/> */}
-            <div className='movie-info-container' style={{
+            <div className='movie-info-overlay' style={{
+                height: infoContainerHeight + 'px'
+            }}></div>
+            <div className='movie-info-container ' ref={infoContainerRef} style={{
                 backgroundImage: 'url('+ window.baseImgUrl + movieData.backdrop +')',
                 backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat' 
+                backgroundRepeat: 'no-repeat',
             }}>
-                <img className='max-h-96 rounded mr-3 shadow shadow-xl' src={window.baseImgUrl + movieData.poster} />
-                <div className='max-w-4xl'>
+                
+
+                <img className='z-10 max-h-96 rounded mr-3 shadow shadow-xl' src={window.baseImgUrl + movieData.poster} />
+                <div className='z-10 max-w-4xl'>
                     <h1 className='text-primary text-2xl font-bold'>{movieData.title} <span className='text-secondary'>({movieData.release_date})</span></h1>
                     <div className='flex items-center text-lg font-medium mt-2'>
                         <img className='w-16 mr-2' src={'images/tmdb.svg'}/>
