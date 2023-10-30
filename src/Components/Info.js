a9a760197a0a6d95c7c7df3935c76085da155da3
Lxgends <marcitis24@gmail.com>
2023-10-30 11:34:40 +0200

    Finished hover ratings

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -17,7 +17,7 @@ export default function Info() {
             
         });
     }, []);
-
+            {/* <div className='movie-search-result' onClick={() => navigate('/review/' + props.id)}></div> */}
     return (
         <>
             <div className="infocontainer">
@@ -25,11 +25,11 @@ export default function Info() {
                         return (
                             <div className="moviereview">
                                 <div className='movieimg'>
-                                    <img src={window.baseImgUrl+x.movie_img}/>
-                                </div>
-                                <div className='img-overlay'>
-                                    <p></p>
-                                    <p></p>
+                                    <div className='img-overlay'>
+                                        <img class="hublogo" src="images\moviehub.png"></img>
+                                        <p className='text-primary font-bold text-xl'>{Math.round(x.rating*10)}%</p>
+                                    </div>
+                                    <img className="reviewimg" src={window.baseImgUrl+x.movie_img}/>
                                 </div>
                                 <p className='text-white'>{x.movie_title} <span className='text-secondary'>({x.movie_year})</span></p>
                             </div>
