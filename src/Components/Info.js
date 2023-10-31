d78f7e44df9f4cf519603c01064bb3f215fc8cdb
Lxgends <marcitis24@gmail.com>
2023-10-31 09:49:32 +0200

    Fixed some issues, added onclick review

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import '../scss/Info.scss';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Pagination } from '@mantine/core';
 
-
-export default function Info() {
+export default function Info(props) {
     const [movieData, setMovieData] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get('get_index_info.php').then((res) => {
             
@@ -17,14 +19,13 @@ export default function Info() {
             
         });
     }, []);
-            {/* <div className='movie-search-result' onClick={() => navigate('/review/' + props.id)}></div> */}
     return (
         <>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
                     {movieData.map((x) => {
                         return (
                             <div className="moviereview">
-                                <div className='movieimg'>
+                                <div className='movieimg' onClick={() => navigate('/review/' + x.tmdb_id)}>
                                     <div className='img-overlay'>
                                         <img class="hublogo" src="images\moviehub.png"></img>
                                         <p className='text-primary font-bold text-xl'>{Math.round(x.rating*10)}%</p>
@@ -32,9 +33,11 @@ export default function Info() {
                                     <img className="reviewimg" src={window.baseImgUrl+x.movie_img}/>
                                 </div>
                                 <p className='text-white'>{x.movie_title} <span className='text-secondary'>({x.movie_year})</span></p>
+
                             </div>
                         );
                     })}
+                <Pagination total={10} color="orange" withControls={false}/>
             </div>
         </>
     )
